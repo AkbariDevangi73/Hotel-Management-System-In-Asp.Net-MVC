33a9ab14297f66dcdf4a05311df979573af99104
devangi <akbaridevangi2000@gmail.com>
2022-05-12 15:18:54 -0400

    Upload

---
+++ ViewModel/BookingViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelManagement.ViewModel
+{
+    public class BookingViewModel
+    {
+        [Display(Name ="Customer Name")]
+        [Required(ErrorMessage ="Customer Name is Required.")]
+        public string CustomerName { get; set; }
+
+        [Display(Name ="Customer Address")]
+        [Required(ErrorMessage = "Customer Address is Required.")]
+        public string CustomerAddress { get; set; }
+
+        [Display(Name ="Customer PhoneNumber")]
+        [Required(ErrorMessage = "Customer PhoneNumber is Required.")]
+        public string CustomerPhone { get; set; }
+
+        [Display(Name ="Booking Form")]
+        //[DataType(DataType.Date)]
+        [Required(ErrorMessage = "Booking From is Required.")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BookingFrom { get; set; }
+
+        [Display(Name ="Booking To")]
+        //[DataType(DataType.Date)]
+        [Required(ErrorMessage = "Booking To is Required.")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BookingTo { get; set; }
+
+        [Display(Name ="Assign Room")]
+        public int AssignRoomId { get; set; }
+
+        [Display(Name ="Number of Members")]
+        [Required(ErrorMessage = "Number of Member is Required.")]
+        public int NoOfMembers { get; set; }
+        public IEnumerable<SelectListItem> ListOfRooms { get; set; }
+
+    }
+}
---
+++ ViewModel/RoomViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelManagement.ViewModel
+{
+    public class RoomViewModel
+    {
+        public int RoomId { get; set; }
+
+        [Display(Name ="Room No.")]
+        [Required(ErrorMessage ="Room Number is required")]
+        public string RoomNumber { get; set; }
+
+        [Display(Name ="Room Image")]
+        public string RoomImage { get; set; }
+
+        [Display(Name ="Room Price")]
+        [Required(ErrorMessage = "Room Price is required")]
+        [Range(500,10000,ErrorMessage ="room price should be equal and greater than {1}")]
+        public decimal RoomPrice { get; set; }
+
+        [Display(Name ="Booking Status")]
+        [Required(ErrorMessage = "Booking Status is required")]
+        public int BookingStatusId { get; set; }
+
+        [Display(Name ="Room Type")]
+        [Required(ErrorMessage = "Room Type is required")]
+        public int RoomTypeId { get; set; }
+
+        [Display(Name ="Room Capacity")]
+        [Required(ErrorMessage = "Room Capacity is required")]
+        [Range(1,8,ErrorMessage ="room capacity should be equal and greater than{1}")]
+        public int RoomCapacity { get; set; }
+
+        public HttpPostedFileBase Image { get; set; }
+
+        [Display(Name ="Room Description")]
+        [Required(ErrorMessage = "Room Description is required")]
+        public string RoomDescription { get; set; }
+
+        public List<SelectListItem> ListOfBookingStatus { get; set; }
+        public List<SelectListItem> ListOfRoomType { get; set; }
+    }
+}
